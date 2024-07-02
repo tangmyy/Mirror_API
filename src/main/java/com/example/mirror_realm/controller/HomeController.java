80e20a86835d4d2ef3494ecae9e17b85892f4637
唐明月 <zx16689532246@163.com>
2024-07-02 08:41:07 +0800

    Test：1
    测试输出id和image

--- src/main/java/com/example/mirror_realm/controller/HomeController.java
+++ src/main/java/com/example/mirror_realm/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
+ 
    @GetMapping("/")
    public String home(Model model) {
       model.addAttribute("message", "Hello, World!");
